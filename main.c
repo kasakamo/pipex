c67b4fdbb535349ac60e68a33898bf0a844af083
Katsuji Sakamoto <kasakamo@c1r5s7.42tokyo.jp>
2025-10-05 18:44:12 +0900

    status

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kasakamo <kasakamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 20:13:41 by kasakamo          #+#    #+#             */
-/*   Updated: 2025/09/17 17:50:25 by kasakamo         ###   ########.fr       */
+/*   Updated: 2025/10/05 17:48:19 by kasakamo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int	main(int ac, char **av, char **envp)
 	int		pipefd[2];
 	pid_t	pid1;
 	pid_t	pid2;
+	int		status;
 
 	if (ac != 5)
 		return (fprintf(stderr, "Usage: %s file1 cmd1 cmd2 file2\n", av[0]), 1);
@@ -30,6 +31,7 @@ int	main(int ac, char **av, char **envp)
 		child2(pipefd, av, envp);
 	close(pipefd[0]);
 	close(pipefd[1]);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
+	waitpid(pid1, &status, 0);
+	waitpid(pid2, &status, 0);
+	return (WEXITSTATUS(status));
 }
